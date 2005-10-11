994b28c927416f877603044e2f312f8596f47b0f
Paolo Borelli <pborelli@src.gnome.org>
2005-10-11 12:33:58 +0000

    whitespaces

--- gedit/gedit-utils.h
+++ gedit/gedit-utils.h
@@ -3,7 +3,8 @@
  * This file is part of gedit
  *
  * Copyright (C) 1998, 1999 Alex Roberts, Evan Lawrence
- * Copyright (C) 2000, 2001 Chema Celorio, Paolo Maggi 
+ * Copyright (C) 2000, 2001 Chema Celorio, Paolo Maggi
+ * Copyright (C) 2002 - 2005 Paolo Maggi
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +23,7 @@
  */
  
 /*
- * Modified by the gedit Team, 1998-2001. See the AUTHORS file for a 
+ * Modified by the gedit Team, 1998-2005. See the AUTHORS file for a 
  * list of people on the gedit Team.  
  * See the ChangeLog files for a list of changes. 
  *
@@ -51,68 +52,76 @@ G_BEGIN_DECLS
 
 enum { GEDIT_ALL_WORKSPACES = 0xffffffff };
 
-gboolean gedit_utils_is_uri_read_only (const gchar* uri);
-gboolean gedit_utils_uri_has_file_scheme (const gchar *uri);
+gboolean	 gedit_utils_is_uri_read_only		(const gchar* uri);
+gboolean	 gedit_utils_uri_has_file_scheme	(const gchar *uri);
 
-void gedit_utils_menu_position_under_widget (GtkMenu  *menu,
-					     gint     *x,
-					     gint     *y,
-					     gboolean *push_in,
-					     gpointer  user_data);
+void		 gedit_utils_menu_position_under_widget (GtkMenu  *menu,
+							 gint     *x,
+							 gint     *y,
+							 gboolean *push_in,
+							 gpointer  user_data);
 
-GtkWidget *gedit_gtk_button_new_with_stock_icon (const gchar *label,
-						 const gchar *stock_id);
+GtkWidget	*gedit_gtk_button_new_with_stock_icon	(const gchar *label,
+							 const gchar *stock_id);
 
-GtkWidget *gedit_dialog_add_button (GtkDialog *dialog, 
-				    const gchar* text, 
-				    const gchar* stock_id, 
-				    gint response_id);
+GtkWidget	*gedit_dialog_add_button		(GtkDialog   *dialog,
+							 const gchar *text,
+							 const gchar *stock_id, 
+							 gint         response_id);
 
-gchar *gedit_utils_str_middle_truncate (const gchar *string, 
-					guint truncate_length);
+gchar		*gedit_utils_str_middle_truncate	(const gchar *string, 
+							 guint        truncate_length);
 
-gboolean g_utf8_caselessnmatch (const char *s1, const char *s2, gssize n1, gssize n2);
+gboolean	 g_utf8_caselessnmatch			(const char *s1,
+							 const char *s2,
+							 gssize n1,
+							 gssize n2);
 
-void gedit_utils_set_atk_name_description (GtkWidget *widget, const gchar *name,
-						const gchar *description);
-void gedit_utils_set_atk_relation (GtkWidget *obj1, GtkWidget *obj2,
-						AtkRelationType rel_type);
+void		 gedit_utils_set_atk_name_description	(GtkWidget  *widget,
+							 const gchar *name,
+							 const gchar *description);
 
-gboolean gedit_utils_uri_exists (const gchar* text_uri);
+void		 gedit_utils_set_atk_relation		(GtkWidget       *obj1,
+							 GtkWidget       *obj2,
+							 AtkRelationType  rel_type);
 
-gchar *gedit_utils_escape_search_text (const gchar *text);
+gboolean	 gedit_utils_uri_exists			(const gchar* text_uri);
 
-gchar *gedit_utils_unescape_search_text (const gchar *text);
+gchar		*gedit_utils_escape_search_text		(const gchar *text);
 
-gchar *gedit_utils_get_stdin (void);
+gchar		*gedit_utils_unescape_search_text	(const gchar *text);
 
-void gedit_warning (GtkWindow *parent, const gchar *format, ...) G_GNUC_PRINTF(2, 3);
+gchar		*gedit_utils_get_stdin			(void);
 
-gchar *gedit_utils_str_middle_truncate (const char *string,
-					guint truncate_length);
+void		gedit_warning				(GtkWindow  *parent,
+							 const gchar *format,
+							 ...) G_GNUC_PRINTF(2, 3);
 
-gchar *gedit_utils_make_valid_utf8 (const char *name);
+gchar		*gedit_utils_str_middle_truncate	(const char *string,
+							 guint truncate_length);
+
+gchar		*gedit_utils_make_valid_utf8		(const char *name);
 
 /* Note that this function replace home dir with ~ */
-gchar *gedit_utils_uri_get_dirname (const char *uri);
+gchar		*gedit_utils_uri_get_dirname		(const char *uri);
 
-gchar *gedit_utils_replace_home_dir_with_tilde (const gchar *uri);
+gchar		*gedit_utils_replace_home_dir_with_tilde (const gchar *uri);
 
-guint gedit_utils_get_current_workspace (GdkScreen *screen);
+guint		 gedit_utils_get_current_workspace	(GdkScreen *screen);
 
-guint gedit_utils_get_window_workspace (GtkWindow *gtkwindow);
+guint		 gedit_utils_get_window_workspace	(GtkWindow *gtkwindow);
 
-void  gedit_utils_activate_url (GtkAboutDialog *about,
-				const gchar    *url,
-				gpointer        data);
+void		 gedit_utils_activate_url		(GtkAboutDialog *about,
+							 const gchar    *url,
+							 gpointer        data);
 
-gboolean gedit_utils_is_valid_uri (const gchar *uri);
+gboolean	 gedit_utils_is_valid_uri		(const gchar *uri);
 
-gboolean gedit_utils_get_glade_widgets (const gchar *filename,
-					const gchar *root_node,
-					GtkWidget **error_widget,
-					const gchar *widget_name,
-					...) G_GNUC_NULL_TERMINATED;
+gboolean	 gedit_utils_get_glade_widgets		(const gchar  *filename,
+							 const gchar  *root_node,
+							 GtkWidget   **error_widget,
+							 const gchar  *widget_name,
+							 ...) G_GNUC_NULL_TERMINATED;
 
 G_END_DECLS
 
