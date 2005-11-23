aa6a421f8b946385f1eadf3ac0fafc1ec194930f
Paolo Borelli <pborelli@src.gnome.org>
2005-11-23 09:47:03 +0000

    fix comment

--- gedit/gedit-tab.h
+++ gedit/gedit-tab.h
@@ -140,10 +140,6 @@ void		 _gedit_tab_print_preview	(GeditTab            *tab,
 						 GeditPrintJob       *pjob,
 						 GtkTextIter         *start, 
 			  			 GtkTextIter         *end);
-
-/*
- * Non exported functions
- */
 void		 _gedit_tab_mark_for_closing	(GeditTab	     *tab);
 
 gboolean	 _gedit_tab_can_close		(GeditTab	     *tab);
