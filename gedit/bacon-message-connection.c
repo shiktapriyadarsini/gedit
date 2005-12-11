a24136cc9aff9532203ca91d11b7918e6dbdf4ef
Paolo Borelli <pborelli@src.gnome.org>
2005-12-11 17:32:51 +0000

    update our copy

--- gedit/bacon-message-connection.c
+++ gedit/bacon-message-connection.c
@@ -65,6 +65,23 @@ test_is_socket (const char *path)
 	return FALSE;
 }
 
+static gboolean
+is_owned_by_user_and_socket (const char *path)
+{
+	struct stat s;
+
+	if (stat (path, &s) == -1)
+		return FALSE;
+
+	if (s.st_uid != geteuid ())
+		return FALSE;
+
+	if ((s.st_mode & S_IFSOCK) != S_IFSOCK)
+		return FALSE;
+	
+	return TRUE;
+}
+
 static gboolean server_cb (GIOChannel *source,
 			   GIOCondition condition, gpointer data);
 
@@ -138,22 +155,67 @@ server_cb (GIOChannel *source, GIOCondition condition, gpointer data)
 }
 
 static char *
-socket_filename (const char *prefix)
+find_file_with_pattern (const char *dir, const char *pattern)
 {
-	char *filename, *path;
-	const char *dir;
+	GDir *filedir;
+	char *found_filename;
+	const char *filename;
+	GPatternSpec *pat;
 
-	filename = g_strdup_printf (".%s.%s", prefix, g_get_user_name ());
+	filedir = g_dir_open (dir, 0, NULL);
+	if (filedir == NULL)
+		return NULL;
 
-	dir = g_getenv ("BACON_SOCKET_DIR");
-	if (dir == NULL)
+	pat = g_pattern_spec_new (pattern);
+	if (pat == NULL)
 	{
-		path = g_build_filename (g_get_home_dir (), filename, NULL);
-	} else {
-		path = g_build_filename (dir, filename, NULL);
+		g_dir_close (filedir);
+		return NULL;
 	}
 
-	g_free (filename);
+	found_filename = NULL;
+
+	while ((filename = g_dir_read_name (filedir)))
+	{
+		if (g_pattern_match_string (pat, filename))
+		{
+			char *tmp = g_build_filename (dir, filename, NULL);
+			if (is_owned_by_user_and_socket (tmp))
+				found_filename = g_strdup (filename);
+			g_free (tmp);
+		}
+
+		if (found_filename != NULL)
+			break;
+	}
+
+	g_pattern_spec_free (pat);
+	g_dir_close (filedir);
+
+	return found_filename;
+}
+
+static char *
+socket_filename (const char *prefix)
+{
+	char *pattern, *newfile, *path, *filename;
+	const char *tmpdir;
+
+	pattern = g_strdup_printf ("%s.%s.*", prefix, g_get_user_name ());
+	tmpdir = g_get_tmp_dir ();
+	filename = find_file_with_pattern (tmpdir, pattern);
+	if (filename == NULL)
+	{
+		newfile = g_strdup_printf ("%s.%s.%u", prefix,
+				g_get_user_name (), g_random_int ());
+		path = g_build_filename (tmpdir, newfile, NULL);
+		g_free (newfile);
+	} else {
+		path = g_build_filename (tmpdir, filename, NULL);
+		g_free (filename);
+	}
+
+	g_free (pattern);
 	return path;
 }
 
