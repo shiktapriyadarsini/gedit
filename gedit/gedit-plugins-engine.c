cd10e1b78cfc8263d6833c86256fcbfe9e46ae83
Ignacio Casal Quinteiro <nacho.resa@gmail.com>
2008-12-22 12:28:08 +0000

    Include gedit-utils.h
    2008-12-22  Ignacio Casal Quinteiro  <nacho.resa@gmail.com>
    
            * gedit/gedit-plugins-engine.c:
            Include gedit-utils.h
    
    
    svn path=/branches/win32/; revision=6682

--- gedit/gedit-plugins-engine.c
+++ gedit/gedit-plugins-engine.c
@@ -44,6 +44,7 @@
 #include "gedit-app.h"
 #include "gedit-plugin-loader.h"
 #include "gedit-object-module.h"
+#include "gedit-utils.h"
 
 #define GEDIT_PLUGINS_ENGINE_BASE_KEY "/apps/gedit-2/plugins"
 #define GEDIT_PLUGINS_ENGINE_KEY GEDIT_PLUGINS_ENGINE_BASE_KEY "/active-plugins"
