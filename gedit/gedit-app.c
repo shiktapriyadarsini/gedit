004652549f94a8da2cd0698ef8f5336dc9167bc1
Jesse van den Kieboom <jessevdk@gnome.org>
2012-01-16 21:28:55 +0100

    Add missing return avlue in process_window_event

--- gedit/gedit-app.c
+++ gedit/gedit-app.c
@@ -1090,8 +1090,10 @@ gedit_app_process_window_event (GeditApp    *app,
 
 	if (GEDIT_APP_GET_CLASS (app)->process_window_event)
 	{
-		GEDIT_APP_GET_CLASS (app)->process_window_event (app, window, event);
+		return GEDIT_APP_GET_CLASS (app)->process_window_event (app, window, event);
 	}
+
+    return FALSE;
 }
 
 static void
