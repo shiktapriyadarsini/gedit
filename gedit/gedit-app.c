f53b2a69a3a216d6827764d383d24cb5e7958c5d
Jesse van den Kieboom <jessevdk@gnome.org>
2012-01-30 16:48:17 +0100

    Added "ready" vfunc for gedit app
    This is needed for OS X to signal the NSApp that
    we are ready to receive events. This needs to be
    done after the main gedit window has been created
    because those events contain things like OpenFile

--- gedit/gedit-app.c
+++ gedit/gedit-app.c
@@ -546,6 +546,11 @@ gedit_app_create_window_impl (GeditApp *app)
 }
 
 static void
+gedit_app_ready_impl (GeditApp *app)
+{
+}
+
+static void
 gedit_app_class_init (GeditAppClass *klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
@@ -562,6 +567,7 @@ gedit_app_class_init (GeditAppClass *klass)
 	klass->set_window_title = gedit_app_set_window_title_impl;
 	klass->quit = gedit_app_quit_impl;
 	klass->create_window = gedit_app_create_window_impl;
+	klass->ready = gedit_app_ready_impl;
 
 	g_object_class_install_property (object_class,
 					 PROP_LOCKDOWN,
@@ -1190,6 +1196,14 @@ _gedit_app_get_settings (GeditApp *app)
 	g_return_val_if_fail (GEDIT_IS_APP (app), NULL);
 
 	return app->priv->settings;
+}
+
+void
+_gedit_app_ready (GeditApp *app)
+{
+	g_return_if_fail (GEDIT_IS_APP (app));
+
+	GEDIT_APP_GET_CLASS (app)->ready (app);
 }
 
 /* ex:set ts=8 noet: */
